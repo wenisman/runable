cf6303d973ca26b43584e23f8c05b32b0ca4d354
Geoffrey Tsai <geoffrey.tsai@gmail.com>
2017-10-17 21:56:13 +1100

    Merged model into master
    * move the swagger to models
    
    * update to make swagger work well, start on the user story
    
    * update so that there are no more unused references
    
    * addition of basic security...

--- events/api/lib/user.js
+++ events/api/lib/user.js
@@ -1,7 +1,10 @@
 const db = require('../lib/mongo');
+const hash = require('object-hash');
 
 const login = (data) => {
-  return db.find('users', {});
+  // make sure we hash the users password
+  let user = db.find('users', { 'login_id': data.login_id });
+  let pwd = hash(data.password);
 };
 
 module.exports = {
